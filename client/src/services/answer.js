bd39e0d45a063107ce3727948529bf36e00b013a
Yash <yash@Yashs-MacBook-Air.local>
2022-01-11 15:16:09 +0530

    AnswerAPI added

---
+++ client/src/services/answer.js
@@ -0,0 +1,13 @@
+import API from './API'
+
+export default {
+  
+  multipleAnswers(data, token)   {
+    return API().post('/answer/multiple', data, { 'headers': { 'Authorization': "Bearer "+token } })
+  },
+
+  getUserAnswers(token)  {
+    return API().get('/answer/user', { 'headers': { 'Authorization': "Bearer "+token } })
+
+  }
+}
